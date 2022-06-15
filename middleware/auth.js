f3976e2d5b862d97ff25955be03ecaaaac77319c
Razija16 <efendirarazija@gmail.com>
2022-06-15 15:44:06 +0200

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,12 @@
+const { decode } = require('jsonwebtoken');
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 // Auth middleware check is user have token
 
 const auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  const token = req.header('Authorization');
+  console.log(token);
 
   if (!token) {
     res.status(401).json({ msg: 'Unauthorized access!' }).end();
@@ -20,4 +23,43 @@ const auth = (req, res, next) => {
   }
 };
 
-module.exports = { auth };
+const adminAuth = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    res.status(401).json({ msg: 'Unauthorized access!' }).end();
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.is_admin == 0) {
+      res.status(403).json({ msg: 'Unauthorized access!' }).end();
+      return;
+    }
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).send('Authorization error!').end();
+  }
+};
+
+const userData = (req, res) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    res.status(401).json({ msg: 'Token Error' }).end();
+    return;
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.user;
+    res.status(201).send(decoded);
+  } catch (err) {
+    res.status(401).send({ message: 'Authorization error!' }).end();
+  }
+  
+}
+
+module.exports = { auth, adminAuth, userData };
--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { auth, userData } = require ('../middleware/auth');
 const { profile,symptoms } = require('../controllers/profile.js');
 
 // router for profile
 router.get ('/forms', symptoms);
 router.get ('/:id', profile);
+router.get ('/test', userData)
 
 
 module.exports = router;
